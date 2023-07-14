cbd7bb557eadd5282e99f0c9f531ee3139770589
sachin-pal <naniw61150@goflipa.com>
2023-07-14 13:40:21 +0530

    Changes to serverfile for left panel, edit and delete;

--- mobilesServer.js
+++ mobilesServer.js
@@ -28,50 +28,52 @@ client.connect(function(res,error){
   console.log("Connected!!!");
 });
 
-
-app.get("/mobiles/:property/:value",function(req,res,next){
-  const property = req.params.property;
-  const value = req.params.value;
-  console.log(`Inside /mobiles/:${property}/:${value}`);
-  const query = `SELECT * FROM mobiles WHERE ${property}=$1`;
-  client.query(query,[value],function(err,result){
-    if(err){
-      console.log(err);
-      return res.status(440).send(err);
-    }
-    return res.send(result.rows);
-    client.end();
-  });
-});
-
-// app.get("/mobiles/rom/:rom",function(req,res,next){
-//   console.log("Inside /mobiles/rom/:rom");
-//   const query = "SELECT * FROM mobiles WHERE rom=$1";
-//   client.query(query,function(err,result){
-//     if(err){
-//       console.log(err);
-//     }
-//     return res.send(result.rows);
-//     client.end();
-//   });
-// });
-
-
 app.get("/mobiles",function(req,res,next){
-    console.log("Inside /mobiles get api");
-    const query = `SELECT * FROM mobiles`;
-    client.query(query,function(err,result){
-        if(err){
-            console.log(err);
-            return res.status(440).send(err);
-        }
-        console.log("ROWS",result.rows);
-        return res.send(result.rows);
-        client.end();
-    });
-});
+  let {brand="", ram="", rom="", os="",sortby=""} = req.query;
+  let query;
+  let params = [];
+  if(brand){
+    brand = brand.split(",").map(b=>"'"+b+"'")
+    params.push("brand IN ("+brand+")");
+  }
+  if(ram){
+    ram = ram.split(",").map(r=>"'"+r+"'")
+    params.push("ram IN ("+ram+")");
+  }
+  if(rom){
+    rom = rom.split(",").map(r=>"'"+r+"'")
+    params.push("rom IN ("+rom+")");
+  }
+  if(os){
+    os = os.split(",").map(o=>"'"+o+"'")
+    params.push("os IN ("+os+")");
+  }
+  params = params.join(" AND ")
+  if(params){
+    if(sortby)
+      query = `SELECT * FROM mobiles WHERE ${params} ORDER BY ${sortby}`;
+    else
+      query = `SELECT * FROM mobiles WHERE ${params}`;
+  }
+  else{
+    if(sortby)
+      query = `SELECT * FROM mobiles ORDER BY ${sortby}`;
+    else
+      query = `SELECT * FROM mobiles`;
+  }
+  console.log(query);
+  client.query(query,function(err,result){
+      if(err){
+          console.log(err);
+          return res.status(440).send(err);
+      }
+      return res.send(result.rows);
+      client.end();
+  });
+})
 
-// INSERT INTO mobiles(name, price, brand, ram, rom, os) VALUES ("iPhone XR",49000,"Apple","4GB","128GB","iOS");
+
+//This method is used to add a mobile in the database.
 
 app.post("/mobile",function(req,res,next){
     console.log("Inside post of mobile");
@@ -86,3 +88,40 @@ app.post("/mobile",function(req,res,next){
         return res.send(`${result.rowCount} insertion successful`);
     })
 })
+
+
+//This method is used to edit the mobile data by mobile name.
+
+app.put("/edit/:name",function(req,res,next){
+  console.log("Inside put of mobiles");
+  let name = req.params.name;
+  let body = req.body;
+  const {price, brand, ram, rom, os} = body;
+  let values = [price,brand,ram,rom,os,name];
+  const query = `UPDATE mobiles SET price=$1, brand=$2, ram=$3, rom=$4, os=$5 WHERE name=$6`;
+  client.query(query,values,function(err,result){
+      if(err){
+          return res.status(400).send(err);
+      }
+      else{
+          return res.send(`${result.rowCount} updation successful`);
+      }
+  });
+});
+
+
+
+//This method is used to delete the mobile from the database by mobile name
+app.delete("/delete/:name",function(req,res,next){
+  console.log("Inside Delete of employees");
+  let name = req.params.name;
+  const query = `DELETE FROM mobiles WHERE name=$1`;
+  client.query(query,[name],function(err,result){
+      if(err){
+          return res.status(400).send(err);
+      }
+      else{
+          return res.send(`${name} deleted`);
+      }
+  });
+});
